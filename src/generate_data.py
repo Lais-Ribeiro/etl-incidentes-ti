216c3b4d867256038ba6558d066d22efd87293a2
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2026-07-24 14:36:26 -0300

    Implementar primeira versão da geração de incidentes fictícios

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,1 +1,18 @@
 # Responsável por gerar dados fictícios de incidentes de TI.
+
+from datetime import datetime
+ 
+def gerar_incidente():
+
+    incidente = {
+        "id_incidente": "INC00001",
+        "data_abertura": "24/07/2026",
+        "sistema": "Portal de Vendas",
+        "status": "Aberto"
+        }
+    return incidente
+
+if __name__ == "__main__":
+
+    resultado = gerar_incidente()
+    print(resultado)
