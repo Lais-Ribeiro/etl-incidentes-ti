16061df1e53d4383e794538e8a8c16245df1571f
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2026-07-25 13:31:11 -0300

    Adiciona geração sequencial de IDs para incidentes

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,18 +1,29 @@
 # Responsável por gerar dados fictícios de incidentes de TI.
 
 from datetime import datetime
- 
-def gerar_incidente():
+
+def gerar_incidente(numero_incidente):
+
+    data_abertura = datetime.now().date().isoformat()
+    id_incidente = f"INC{numero_incidente:06d}"
 
     incidente = {
-        "id_incidente": "INC00001",
-        "data_abertura": "24/07/2026",
+        "id_incidente": id_incidente,
+        "data_abertura": data_abertura,
         "sistema": "Portal de Vendas",
         "status": "Aberto"
         }
     return incidente
 
+def gerar_incidentes(quantidade):
+
+    lista_incidentes = []
+
+    for i in range(quantidade):
+            lista_incidentes.append(gerar_incidente(i + 1))
+    return lista_incidentes
+
 if __name__ == "__main__":
 
-    resultado = gerar_incidente()
+    resultado = gerar_incidentes(2)
     print(resultado)
