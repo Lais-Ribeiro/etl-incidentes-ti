0bd5672f6aa3fddf4703134d3b8310fb191e5a23
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2026-07-25 18:29:06 -0300

    Finaliza geração completa de dados fictícios para incidentes

--- src/generate_data.py
+++ src/generate_data.py
@@ -7,16 +7,14 @@ import random
 
 def gerar_sistema():
 
-    sistemas = [
+    lista_sistemas = [
          "Portal de Vendas",
          "API de pagamentos",
          "Autenticação",
          "Catálogo de Produtos"
     ]
 
-    sistema = random.choice(sistemas)
-
-    return sistema
+    return random.choice(lista_sistemas)
 
 def gerar_status():
 
@@ -27,8 +25,72 @@ def gerar_status():
          "Em espera"
     ]
 
-    status = random.choice(lista_status)
-    return status
+    return random.choice(lista_status)
+
+def gerar_categoria():
+
+    lista_categorias = [
+         "Infraestrutura",
+         "Banco de dados",
+         "Rede",
+         "Aplicação"
+    ]
+
+    return random.choice(lista_categorias)
+
+def gerar_severridade():
+
+    lista_severidade = [
+          "Baixa",
+          "Média",
+          "Alta",
+          "Crítica"
+     ]
+    
+    pesos = [50,30,15,5]
+
+    severidade = random.choices(lista_severidade,weights=pesos)
+    return severidade[0]
+
+def gerar_resposnsavel():
+
+    lista_responsavel = [
+          "Maria Silva",
+          "Carlos Macedo",
+          "Guilherme Silverio",
+          "Matheus Ferreira",
+          "Nathalia Moreira",
+          "Juliana Mendonça"
+     ]
+
+    return random.choice(lista_responsavel)
+
+def gerar_descricao():
+
+    lista_descricao = [
+        "Usuário não consegue acessar o sistema",
+        "Erro ao processar pagamento",
+        "Banco de dados indisponível",
+        "Timeout na API de autenticação",
+        "Lentidão no poral de vendas"
+    ]
+
+    descricao = random.choice(lista_descricao)
+    return descricao
+
+def gerar_prioridade():
+
+    lista_prioridades = [
+        "Baixa",
+        "Média",
+        "Alta"
+    ]
+
+    pesos = [60,30,10]
+
+    prioridade = random.choices(lista_prioridades,weights=pesos)
+
+    return prioridade[0]
 
 def gerar_data_abertura():
 
@@ -58,13 +120,23 @@ def gerar_incidente(numero_incidente):
     sistema = gerar_sistema()
     status = gerar_status()
     data_fechamento = gerar_data_fechamento(data_abertura, status)
+    categoria = gerar_categoria()
+    severidade = gerar_severridade()
+    resposnavel = gerar_resposnsavel()
+    descricao = gerar_descricao()
+    prioridade = gerar_prioridade()
 
     incidente = {
         "id_incidente": id_incidente,
         "data_abertura": data_abertura.isoformat(),
         "sistema": sistema,
         "status": status,
-        "data_fechamento": (data_fechamento.isoformat() if data_fechamento else None)
+        "data_fechamento": (data_fechamento.isoformat() if data_fechamento else None),
+        "categoria": categoria,
+        "severidade": severidade,
+        "responsavel": resposnavel,
+        "descricao": descricao,
+        "prioridade": prioridade
         }
     return incidente
 
@@ -74,11 +146,11 @@ def gerar_incidentes(quantidade):
     lista_incidentes = []
 
     for i in range(quantidade):
-        lista_incidentes.append(gerar_incidente(i + 1))
+            lista_incidentes.append(gerar_incidente(i + 1))
     return lista_incidentes
 
 # Ponto de entrada do programa
 if __name__ == "__main__":
 
-    resultado = gerar_incidentes(2)
+    resultado = gerar_incidentes(1)
     print(resultado)
